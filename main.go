c7c11f6be3c96c19d378d0473f85bc78d4d3362f
adetunjii <adetunjithomas1@outlook.com>
2022-05-03 17:07:44 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type inputFile struct {
+	filePath  string
+	separator string
+	pretty    bool
+}
+
+func main() {
+	fmt.Println("Hello world")
+	checkIfValidFile("test.csv")
+}
+
+func getFileData() (inputFile, error) {
+	if len(os.Args) < 2 {
+		return inputFile{}, errors.New("A filepath argument is required")
+	}
+
+	separator := flag.String("separator", "comma", "column separator")
+	pretty := flag.Bool("pretty", false, "Generate pretty JSON")
+
+	flag.Parse()
+
+	fileLocation := flag.Arg(0)
+
+	if !(*separator == "comma" || *separator == "semicolon") {
+		return inputFile{}, errors.New("only comma or semicolon separators are allowed")
+	}
+
+	return inputFile{fileLocation, *separator, *pretty}, nil
+}
+
+func checkIfValidFile(filename string) (bool, error) {
+
+	if fileExtension := filepath.Ext(filename); fileExtension != ".csv" {
+		return false, fmt.Errorf("file %s is not CSV", filename)
+	}
+
+	//os.Stat checks if the entered filepath is an existing file.
+	if _, err := os.Stat(filename); err != nil && os.IsNotExist(err) {
+		return false, fmt.Errorf("file %s does not exist", filename)
+	}
+
+	return true, nil
+}
+
+//func processCsvFile(fileData inputFile, writerChannel chan<- map[string]string) {
+//
+//	// open the file
+//	file, err := os.Open(fileData.filePath)
+//	if err != nil {
+//		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+//		os.Exit(1)
+//	}
+//
+//	// close the file when the function returns
+//	defer func() {
+//		err := file.Close()
+//		if err != nil {
+//			fmt.Fprintf(os.Stderr, "error %v\n", err)
+//			os.Exit(1)
+//		}
+//	}()
+//
+//	var headers, line []string
+//	reader := csv.NewReader(file)
+//
+//	// the default separator for csv is comma, so we change it semicolon is specified
+//	if fileData.separator == "semicolon" {
+//		reader.Comma = ';'
+//	}
+//
+//	// reading the file for the first time would fetch the header row
+//	headers, err = reader.Read()
+//	if err != nil {
+//		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+//		os.Exit(1)
+//	}
+//
+//	// keep fetching each row then send it to a writerChannel
+//	// the loop stops when we reach the end of the file
+//	for {
+//		line, err = reader.Read()
+//
+//		if err != nil {
+//			if err == io.EOF {
+//				close(writerChannel)
+//				break
+//			}
+//			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+//			os.Exit(1)
+//		}
+//
+//		record, err := processLine(headers, line)
+//		if err != nil {
+//			fmt.Printf("Line: %sError: %s\n", line, err)
+//			continue
+//		}
+//
+//		writerChannel <- record
+//	}
+//}
+
+//func processLine(headers, line []string) (map[string]string, error) {
+//	if len(line) != len(headers) {
+//		return nil, errors.New("line does not match header")
+//	}
+//
+//	recordMap := make(map[string]string)
+//
+//	for i, name := range headers {
+//		recordMap[name] = line[i]
+//	}
+//	return recordMap, nil
+//}
+
+//func writeJSONFile(csvPath string, writerChannel <-chan map[string]string, done chan<- bool, pretty bool) {
+//
+//
+//
+//	_, err := jsonFile.WriteString("["+breakLine, )
+//
+//}
+//
+//func createStringWriter(csvPath string) func(string, bool) {
+//	jsonDir := filepath.Dir(csvPath)
+//	jsonName := fmt.Sprintf("%s.json", strings.TrimSuffix(filepath.Base(csvPath), ".csv"))
+//	jsonLocation := filepath.Join(jsonDir, jsonName)
+//
+//	jsonFile, err := os.Create(jsonLocation)
+//	if err != nil {
+//		fmt.Fprintf(os.Stderr, "error occured %v ", err)
+//		os.Exit(1)
+//	}
+//}
---
+++ main_test.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"flag"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestGetFileData(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected inputFile
+		err      bool
+		osArgs   []string
+	}{
+		{"Default parameters", inputFile{"test.csv", "comma", false}, false, []string{"cmd", "test.csv"}},
+		{"No parameters", inputFile{}, true, []string{"cmd"}},
+		{"Semicolon enabled", inputFile{"test.csv", "semicolon", false}, false, []string{"cmd", "--separator=semicolon", "test.csv"}},
+		{"Pretty enabled", inputFile{"test.csv", "comma", true}, false, []string{"cmd", "--pretty", "test.csv"}},
+		{"Pretty and semicolon enabled", inputFile{"test.csv", "semicolon", true}, false, []string{"cmd", "--pretty", "--separator=semicolon", "test.csv"}},
+		{"Separator not identified", inputFile{}, true, []string{"cmd", "--separator=pipe", "test.csv"}},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			actualOsArgs := os.Args
+
+			defer func() {
+				os.Args = actualOsArgs
+				flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+			}()
+
+			os.Args = testCase.osArgs
+			inputFile, err := getFileData()
+
+			if (err != nil) != testCase.err {
+				t.Errorf("getFileData() error = %v, wantErr %v", err, testCase.err)
+				return
+			}
+
+			require.Equal(t, testCase.expected, inputFile)
+
+		})
+	}
+}
+
+func TestCheckIfValidFile(t *testing.T) {
+
+	tempFile, err := os.CreateTemp("", "test*.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(name string) {
+		err := os.Remove(name)
+		if err != nil {
+
+		}
+	}(tempFile.Name())
+
+	tests := []struct {
+		name     string
+		filename string
+		expected bool
+		err      bool
+	}{
+		{"File does exist", tempFile.Name(), true, false},
+		{"File does not exist", "nowhere/test.csv", false, true},
+		{"File is not csv", "test.txt", false, true},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			isValid, err := checkIfValidFile(testCase.filename)
+
+			if (err != nil) != testCase.err {
+				t.Errorf("File %v is invalid", testCase.filename)
+				return
+			}
+
+			require.Equal(t, testCase.expected, isValid)
+		})
+	}
+}
+
+//func TestProcessCsvFile(t *testing.T) {
+//	expectedMapSlice := []map[string]string{
+//		{"id": "1", "name": "samuel", "age": "25", "email": "adetunjithomas1@gmail.com"},
+//	}
+//
+//	tests := []struct {
+//		name      string
+//		csvString string
+//		separator string
+//	}{
+//		{"Comma separator", "id,name,age,email\n1,samuel,25,adetunjithomas1@gmail.com", "comma"},
+//		{"Semicolon separator", "id;name;age;email\n1;samuel;25;adetunjithomas1@gmail.com", "semicolon"},
+//	}
+//
+//	for _, testCase := range tests {
+//		t.Run(testCase.name, func(t *testing.T) {
+//			tmpFile, err := os.CreateTemp("", "test*.csv")
+//			defer func(name string) {
+//				err := os.Remove(name)
+//				require.NoError(t, err)
+//			}(tmpFile.Name())
+//
+//			require.NoError(t, err)
+//
+//			_, err = tmpFile.WriteString(testCase.csvString)
+//
+//			// moves the tmpFile from memory to disk
+//			err = tmpFile.Sync()
+//			require.NoError(t, err)
+//
+//			testFileData := inputFile{
+//				filePath:  tmpFile.Name(),
+//				pretty:    false,
+//				separator: testCase.separator,
+//			}
+//
+//			writerChannel := make(chan map[string]string)
+//
+//			go processCsvFile(testFileData, writerChannel)
+//
+//			for _, expectedMap := range expectedMapSlice {
+//				record := <-writerChannel
+//				require.Equal(t, expectedMap, record)
+//			}
+//		})
+//	}
+//
+//}
